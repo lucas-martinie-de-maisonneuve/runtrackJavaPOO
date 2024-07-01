4a3fef59fc2aaab185eb5e8ee857f46e7f8ecd27
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-07-01 10:57:45 +0200

    <terminé> jour06.job05 - Saisir un nombre et le mettre au carré

---
+++ jour06/job05/src/Main04.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Quel chiffre à mettre au carré ?");
+        int nombre = input.nextInt();
+        input.close();
+        System.out.println("Le carré de " + nombre +" est : " + nombre * nombre);
+    }
+}
