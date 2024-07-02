2e4647805120ad465d062018154ef48e1b334e79
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-07-02 13:14:15 +0200

    <terminé> jour07.job3.0 - Creation d'un objet Rectangle

--- jour07/job01/src/Main01.java
+++ jour07/job01/src/Main01.java
@@ -8,7 +8,7 @@ class Toto {
     public static void main(String[] tutu) {
         Toto t1 = new Toto();
         Toto t2 = new Toto();
-        System.out.println("Toto t1: " + toto);
+        // System.out.println("Toto t1: " + toto);
     }
 }
 
---
+++ jour07/job3.1/src/Main30.java
@@ -0,0 +1,7 @@
+public class Main30 {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5.0, 10.0);
+        System.out.println(rectangle.getLongueur());
+        System.out.println(rectangle.surface());
+    }
+}
