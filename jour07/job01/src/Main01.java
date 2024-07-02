1f3b0bd1ce9ea17be8424bcc5677eb254e1902a9
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-07-02 12:33:55 +0200

    <terminé> jour07.job01 - Erreur d'instanciation

--- jour07/job01/src/Main01.java
+++ jour07/job01/src/Main01.java
@@ -1,5 +1,6 @@
 class Toto {
     int toto = 0;
+
     Toto() {
         toto = toto + 1;
     }
@@ -7,7 +8,12 @@ class Toto {
     public static void main(String[] tutu) {
         Toto t1 = new Toto();
         Toto t2 = new Toto();
-        System.out.println("Toto t1: " + t1.toto);
-        System.out.println("Toto t2: " + t2.toto);
+        System.out.println("Toto t1: " + toto);
     }
 }
+
+/*
+ * toto n'est pas lié à son instance (t1 ou t2)
+ * Il aurait fallu corriger de la sorte : 
+ * System.out.println("Toto t1: " + t1.toto);
+ */
