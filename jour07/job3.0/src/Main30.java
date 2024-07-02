ca89343437712569278a902ce8a0fc562bf73e8a
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-07-02 13:55:56 +0200

    <terminé> jour07.job3.1 - Creation de la classe cercle

--- jour07/job3.0/src/Main30.java
+++ jour07/job3.0/src/Main30.java
@@ -1,7 +1,7 @@
 public class Main30 {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(5.0, 10.0);
-        System.out.println(rectangle.getLongueur());
-        System.out.println(rectangle.surface());
+        System.out.println("Longueur du rectangle : " + rectangle.getLongueur());
+        System.out.println("Largeur du rectangle : " + rectangle.surface());
     }
 }
--- jour07/job3.1/src/Main30.java
+++
@@ -1,7 +0,0 @@
-public class Main30 {
-    public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(5.0, 10.0);
-        System.out.println(rectangle.getLongueur());
-        System.out.println(rectangle.surface());
-    }
-}
